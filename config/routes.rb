7e5146c60019bb0660f9da55c73dc0979e27c4fb
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-28 20:35:35 +0300

    made views and routes for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	resources :questions
+  resources :questions do
+    resources :answers, shallow: true, only: %i[new create show]
+  end
 end
