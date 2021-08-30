cce8a92818684c943a8e1c5e91c98a4619f3e004
Андрей Румянцев <Wertyos111@gmail.com>
2021-08-30 14:50:04 +0300

    made best answer in db

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@
 
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  belongs_to :best_answer, class_name: 'Answer', foreign_key: :best_answer_id, optional: true
   belongs_to :user
 
   validates :title, :body, presence: true
