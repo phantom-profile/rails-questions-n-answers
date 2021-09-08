daa137e0e1f5ef209915ce0d80e8d7f946c18721
Андрей Румянцев <Wertyos111@gmail.com>
2021-09-08 17:22:56 +0300

    Created Link with validated url

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,11 +2,13 @@
 
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  has_many :links, as: :linkable, dependent: :destroy
 
   has_many_attached :files
 
   belongs_to :best_answer, class_name: 'Answer', foreign_key: :best_answer_id, optional: true
   belongs_to :user
 
+  accepts_nested_attributes_for :links, reject_if: :all_blank
   validates :title, :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   end
 
   resources :attachments, only: %i[destroy]
+  resources :links, only: %i[destroy]
 end
