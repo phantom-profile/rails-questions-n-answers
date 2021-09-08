57f2d3bb01c4ab611f1a080749ee47b071cfc611
Андрей Румянцев <Wertyos111@gmail.com>
2021-09-08 21:16:24 +0300

    user can see reward list

---
+++ spec/controllers/reward_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe RewardsController, type: :controller do
+  let(:rewards) { create_list(:reward, 5) }
+  let(:user) { create(:user, rewards: rewards) }
+
+  describe "GET #index" do
+    before do
+      login(user)
+      get :index
+    end
+
+    it 'gets all current user rewards from DB' do
+      expect(assigns(:rewards)).to match_array rewards
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+end
