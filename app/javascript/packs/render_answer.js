07f42bab9b60a2ab27c745aacbb98ad80ce872a4
Андрей Румянцев <Wertyos111@gmail.com>
2021-09-14 16:03:10 +0300

    removed logs

--- app/javascript/packs/render_answer.js
+++ app/javascript/packs/render_answer.js
@@ -1,14 +1,13 @@
 $(document).on('turbolinks:load', function () {
-    $('form.new-answer').on('ajax:success', function(e) {
-        console.log(e.detail)
-        let answer = e.detail[0]
+  $('form.new-answer').on('ajax:success', function(e) {
+    let answer = e.detail[0]
 
-        $('.answers').append(`<p> ${answer.body} </p>`)
+    $('.answers').append(`<p> ${answer.body} </p>`)
+  })
+    .on('ajax:error', function (e) {
+      let errors = e.detail[0]
+      $.each(errors, function (index, value) {
+        $('.answer-errors').html('').append(`<p> ${value} </p>`)
+      })
     })
-        .on('ajax:error', function (e) {
-            let errors = e.detail[0]
-            $.each(errors, function (index, value) {
-                $('.answer-errors').html('').append(`<p> ${value} </p>`)
-            })
-        })
-})
+  })
--- app/javascript/packs/render_voting_panel.js
+++ app/javascript/packs/render_voting_panel.js
@@ -1,6 +1,5 @@
 $(document).on('turbolinks:load', function () {
   $('.voting-panel').on('ajax:success', function(e) {
-    console.log(e.detail)
     let voting = e.detail[0]
     let votingPanel = $(`.voting-panel.${voting.resource_id}`)
     votingPanel.find('.vote-statistic').html(`<p>Question rating</p><p>${voting.rating}</p>`)
