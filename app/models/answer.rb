99bf374d7c8b55877ba4d556023b2ef16b5587aa
Андрей Румянцев <Wertyos111@gmail.com>
2021-09-15 16:54:19 +0300

    model style

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,14 +5,14 @@ class Answer < ApplicationRecord
 
   include IsVotable
 
-  has_many_attached :files
-  has_many :links, as: :linkable, dependent: :destroy
-
   belongs_to :question
   belongs_to :user
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
+  has_many_attached :files
+  has_many :links, as: :linkable, dependent: :destroy
+
   validates :body, :question, presence: true
 
+  accepts_nested_attributes_for :links, reject_if: :all_blan
   scope :without_best, ->(answer) { where.not(id: answer) }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,18 +3,19 @@
 class Question < ApplicationRecord
   include IsVotable
 
+  belongs_to :best_answer, class_name: 'Answer', foreign_key: :best_answer_id, optional: true
+  belongs_to :user
+
   has_many :answers, dependent: :destroy
   has_many :links, as: :linkable, dependent: :destroy
   has_one :reward, class_name: 'Reward', dependent: :destroy
 
   has_many_attached :files
 
-  belongs_to :best_answer, class_name: 'Answer', foreign_key: :best_answer_id, optional: true
-  belongs_to :user
+  validates :title, :body, presence: true
 
   accepts_nested_attributes_for :links, reject_if: :all_blank
   accepts_nested_attributes_for :reward, reject_if: :all_blank
-  validates :title, :body, presence: true
 
   def choose_best_answer(answer, user)
     update!(best_answer: answer)
